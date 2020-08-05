3fff45ce236e5ef19b53942120c947a0a89a5d33
Edson <edson.rodgon@gmail.com>
2020-08-05 09:30:12 -0300

    whatever, you know

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+mongoose.connect('')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 80
+
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+app.use(cors())
+
+
+app.listen(port)
